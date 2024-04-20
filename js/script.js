077c125546a32877004cf8e8bbc622c60bf9b1c9
ezequiel frank <eze69lp@gmail.com>
2024-04-20 12:57:48 -0300

    version final

--- js/script.js
+++ js/script.js
@@ -12,3 +12,16 @@ const header = document.querySelector("[data-header]");
 window.addEventListener("scroll", function () {
   header.classList[this.scrollY > 50 ? "add" : "remove"]("active");
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+  var slides = document.querySelectorAll(".slide");
+  var currentSlide = 0;
+  
+  function nextSlide() {
+    slides[currentSlide].style.display = "none";
+    currentSlide = (currentSlide + 1) % slides.length;
+    slides[currentSlide].style.display = "block";
+  }
+  
+  setInterval(nextSlide, 3000); // Cambiar de imagen cada 3 segundos (3000 milisegundos)
+});
